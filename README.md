182936960857c45a25aecc8eb9694103bb27af06
gabriel.matsubara@gmail.com <gabriel.matsubara@gmail.com>
2023-08-11 16:08:26 -0300

    Mudanças no projeto

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
 ## Projeto Modelo para Atividade Java X GitHub
 
-Sejam bem-vindos ao projeto.
+Bem vindo ao projeto!
 
 ## Enunciado
 
-Tendo como dados de entrada a altura de uma pessoa, construa um algoritmo que calcule seu peso ideal, usando a seguinte fórmula: 
-(72.7*altura) - 58.
+Tendo como dados de entrada a altura de uma pessoa, construa um algoritmo que calcule seu peso ideal, usando a seguinte fórmula:
+
+"(72.7 * altura) - 58".
 
 ## Dependency Management
 
--- src/App.java
+++ src/App.java
@@ -4,9 +4,9 @@ public class App {
     public static void main(String[] args) throws Exception {
         try (Scanner entrada = new Scanner(System.in)) {
             System.out.print("Digite sua altura: ");
-            double h = entrada.nextDouble();
-            double w = ((72.7 * h) - 58);
-            System.out.println("Seu peso ideal é : " + w + "Kg");
+            double altura = entrada.nextDouble();
+            double peso = ((72.7 * altura) - 58);
+            System.out.println("Seu peso ideal é : " + peso + "Kg");
         } 
     }
 }
